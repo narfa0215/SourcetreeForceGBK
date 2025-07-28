af8f0a8069393699f38ecc7058795a699f27219c
narfa0215 <124590168+narfa0215@users.noreply.github.com>
2025-07-28 12:38:35 +0800

    Force GBK encoding for projects with APP.ini or .project, with patch generated from original file

--- SourcetreeForceGBK/Program.cs
+++ SourcetreeForceGBK/Program.cs
@@ -25,16 +25,32 @@ namespace SourcetreeForceGBK
             }
             Console.WriteLine($"请输入需要打补丁的SourceTree.Api.UI.Wpf.dll的路径(可拖入dll文件)(默认：{targetFilePath}，回车使用默认)：");
             var fileName = Console.ReadLine();
-            fileName = string.IsNullOrWhiteSpace(fileName) ? targetFilePath : fileName;
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                if (string.IsNullOrWhiteSpace(targetFilePath))
+                {
+                    Console.WriteLine("默认SourceTree.Api.UI.Wpf.dll路径不可用！");
+                    Console.ReadKey();
+                    return;
+                }
+                fileName = targetFilePath;
+            }
+
+            var loadFileName = fileName;
+            if (File.Exists(fileName + ".bak"))
+            {
+                loadFileName += ".bak";
+            }
             // 加载目标程序集（可以是任何 .exe 或 .dll 文件）
-            var module = ModuleDefMD.Load(fileName);  // 目标程序集
+            var module = ModuleDefMD.Load(loadFileName);  // 目标程序集
             var processType = module.Types.First(t => "SourceTree.Utils.RepoProcess".Equals(t.FullName)); // 替换成目标类名
             var method = processType.Methods.First(m => "System.Void SourceTree.Utils.RepoProcess::InternalLaunch(System.Boolean,System.Diagnostics.ProcessPriorityClass)".Equals(m.FullName));
             var instructions = method.Body.Instructions;
 
-            var isPatch = instructions.Any(i => OpCodes.Ldstr.Equals(i.OpCode) && " -- ".Equals(i.Operand));
+            var isPatch1 = instructions.Any(i => OpCodes.Ldstr.Equals(i.OpCode) && " -- ".Equals(i.Operand));
+            var isPatch2 = instructions.Any(i => OpCodes.Ldstr.Equals(i.OpCode) && ("APP.ini".Equals(i.Operand) || ".project".Equals(i.Operand)));
 
-            if (isPatch)
+            if (isPatch1 && isPatch2)
             {
                 Console.WriteLine("已打完补丁，不用重复打补丁！");
                 Console.ReadKey();
@@ -62,6 +78,8 @@ namespace SourcetreeForceGBK
                 return;
             }
 
+            var instructionEncodingStart = Instruction.Create(OpCodes.Ldloc_1);
+
             // 构造插入的 IL 指令
             var insertInstructions = new Instruction[]
             {
@@ -80,10 +98,30 @@ namespace SourcetreeForceGBK
                 Instruction.Create(OpCodes.Ldstr, " -- "),  // 加载字符串 " -- "
                 Instruction.Create(OpCodes.Callvirt, module.Import(typeof(string).GetMethod("Contains", new[] { typeof(string) }))),
                 Instruction.Create(OpCodes.Brfalse_S, instructionEnd), // 如果 Arguments 不包含 " -- "，跳过后面的逻辑
+                
+                // if (File.Exists(Path.Combine(process.StartInfo.WorkingDirectory, "APP.ini")))
+                Instruction.Create(OpCodes.Nop), // Nop
+                Instruction.Create(OpCodes.Ldloc_1), // 加载 `process`
+                Instruction.Create(OpCodes.Callvirt, module.Import(typeof(System.Diagnostics.Process).GetProperty("StartInfo").GetGetMethod())),
+                Instruction.Create(OpCodes.Callvirt, module.Import(typeof(System.Diagnostics.ProcessStartInfo).GetProperty("WorkingDirectory").GetGetMethod())),
+                Instruction.Create(OpCodes.Ldstr, "APP.ini"),  // 加载字符串 "APP.ini"
+                Instruction.Create(OpCodes.Call, module.Import(typeof(Path).GetMethod("Combine", new[] { typeof(string), typeof(string) }))),
+                Instruction.Create(OpCodes.Call, module.Import(typeof(File).GetMethod("Exists", new[] { typeof(string) }))),
+                Instruction.Create(OpCodes.Brtrue_S, instructionEncodingStart), // 如果 APP.ini 存在，继续执行
+                
+                // if (File.Exists(Path.Combine(process.StartInfo.WorkingDirectory, ".project")))
+                Instruction.Create(OpCodes.Nop), // Nop
+                Instruction.Create(OpCodes.Ldloc_1), // 加载 `process`
+                Instruction.Create(OpCodes.Callvirt, module.Import(typeof(System.Diagnostics.Process).GetProperty("StartInfo").GetGetMethod())),
+                Instruction.Create(OpCodes.Callvirt, module.Import(typeof(System.Diagnostics.ProcessStartInfo).GetProperty("WorkingDirectory").GetGetMethod())),
+                Instruction.Create(OpCodes.Ldstr, ".project"),  // 加载字符串 ".project"
+                Instruction.Create(OpCodes.Call, module.Import(typeof(Path).GetMethod("Combine", new[] { typeof(string), typeof(string) }))),
+                Instruction.Create(OpCodes.Call, module.Import(typeof(File).GetMethod("Exists", new[] { typeof(string) }))),
+                Instruction.Create(OpCodes.Brfalse_S, instructionEnd), // 如果 .project 存在，继续执行
             
                 // process.StartInfo.StandardOutputEncoding = Encoding.Default;
                 Instruction.Create(OpCodes.Nop), // Nop
-                Instruction.Create(OpCodes.Ldloc_1), // 加载 `process`
+                instructionEncodingStart, // 加载 `process`
                 Instruction.Create(OpCodes.Callvirt, module.Import(typeof(System.Diagnostics.Process).GetProperty("StartInfo").GetGetMethod())),
                 Instruction.Create(OpCodes.Call, module.Import(typeof(System.Text.Encoding).GetProperty("Default").GetGetMethod())), // 获取 Encoding.Default
                 Instruction.Create(OpCodes.Callvirt, module.Import(typeof(System.Diagnostics.ProcessStartInfo).GetProperty("StandardOutputEncoding").GetSetMethod())),
@@ -107,8 +145,10 @@ namespace SourcetreeForceGBK
             method.Body.OptimizeBranches();
             
             // 原始dll文件重命名保留
-            File.Move(fileName, fileName + ".bak");
-            
+            if (!loadFileName.EndsWith(".bak") && !isPatch1)
+            {
+                File.Move(fileName, fileName + ".bak");
+            }
             // 保存修改后的程序集
             module.Write(fileName);  // 保存为新的文件
             Console.WriteLine("补丁已成功打上！");
